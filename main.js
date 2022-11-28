160de24aabc3e36707f5fadc9ba9d25c8997f6d5
sanyamkhera <93199788+sanyamkhera@users.noreply.github.com>
2022-11-28 16:42:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup(){
+canvas=createCanvas(600,500);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+}
+song="";
+ function draw(){
+    image(video,00,600,500);
+
+ }    
+ 
